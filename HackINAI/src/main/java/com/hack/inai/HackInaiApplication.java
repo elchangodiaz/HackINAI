e3f02e14d9de08889a2a2db70051c67289dba725
elchangodiaz <elchangodiaz1@gmail.com>
2021-05-18 19:45:08 -0500

    add spring proyect

---
+++ HackINAI/src/main/java/com/hack/inai/HackInaiApplication.java
@@ -0,0 +1,13 @@
+package com.hack.inai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackInaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackInaiApplication.class, args);
+	}
+
+}
