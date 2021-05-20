f27ba63214991ec61494c00733f7ecbdc2359a9c
unknown <e2g369@gmail.com>
2021-05-20 11:13:32 -0500

    mas graficas

--- ini/src/app/components/filtrado-ubicacion/filtrado-ubicacion.component.ts
+++ ini/src/app/components/filtrado-ubicacion/filtrado-ubicacion.component.ts
@@ -9,6 +9,27 @@ export class FiltradoUbicacionComponent implements OnInit {
 
   data: string[] = [];
   alcaldiaSalarios: any[] = [];
+  public nombres = [
+    'Álvaro Obregón',
+    'Azcapotzalco',
+    'Benito Juárez',
+    'Coyoacán',
+    'cuajimalpa',
+    'Cuahutémoc',
+    'Gustavo A. Madero',
+    'Iztacalco',
+    'Iztapalapa',
+    'Magdalena Contreras',
+    'Miguel Hidalgo',
+    'Milpa Alta',
+    'Tláhuac',
+    'Tlalpan',
+    'Venustiano Carranza',
+    'Xochimilco'
+
+]
+public Data = [0,1,0,0,0,9086,0,4099,0,0,1,1,1,1,1,1];
+public doughnutChartType: string = 'doughnut';
   constructor( private filtro: FiltroUbicacion) { }
 
   ngOnInit(): void {
