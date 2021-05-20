1e2c65bb58749571480175dc8c9f6b4d4f27f1be
unknown <e2g369@gmail.com>
2021-05-19 20:10:00 -0500

    acceso a datos, mostrar datos e inicio de trabajo de datos

--- ini/src/app/components/salarios/salarios.component.ts
+++ ini/src/app/components/salarios/salarios.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Service} from '../../service';
+import {getCsv} from '../../services/getCsv';
+import {TransformSalario} from '../../services/transformSalarios';
 @Component({
   selector: 'app-salarios',
   templateUrl: './salarios.component.html',
@@ -7,17 +8,25 @@ import {Service} from '../../service';
 })
 export class SalariosComponent implements OnInit {
   data: any = [];
-  constructor(private service: Service) {}
+  urlTrimestre = 'https://raw.githubusercontent.com/Erick2G/Hack2021/main/csvs/primer_trim_concurso.csv';
+  constructor(private getcsv: getCsv, private t:TransformSalario) {}
 
   ngOnInit(): void {
     this.getData();
   }
   getData(){
-    this.service.getInfo().subscribe(data =>{
-      const list = data.split('\n');
-      list.forEach(e =>{
-        this.data.push(e);
-      })
+    this.getcsv.getInfo(this.urlTrimestre).subscribe(infos =>{
+      const list = infos.split('\n');
+      this.data = this.t.transform(list);
+      /*list.forEach(e =>{
+        let temp = e.split(',');
+        this.data.push(temp);
+      })*/
     })
+
+    
   }
+
 }
+
+
--- ini/src/app/services/getCsv.ts
+++ ini/src/app/services/getCsv.ts
@@ -1,19 +1,14 @@
 import {Injectable} from '@angular/core';
-//import { HttpModule } from '@angular/http';
 import {HttpClient} from '@angular/common/http';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class Service{
+export class getCsv{
   data = 'https://raw.githubusercontent.com/Erick2G/Hack2021/main/csvs/primer_trim_concurso.csv';
   constructor(private http: HttpClient){}
-  getInfo(){
-    console.log('lista: ');
-    const lista = this.http.get(this.data,{responseType:'text'});
-
-    console.log(lista);
+  getInfo( dir:string){
+    const lista = this.http.get(dir,{responseType:'text'});
     return lista;
   }
 }
